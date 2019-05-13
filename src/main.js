a91831eb064776865b978459779d49eb3ecda4b7
gian <gf.fioriello@gmail.com>
2019-05-13 13:14:29 -0300

    Add explanation to the eventBus line in the main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+/*
+- NOTE that the you need to include the 
+ event bus to you main.js in order to have 
+ a correct integration between the carousel 
+ and its items 
+*/ 
 Vue.prototype.$eventBus = new Vue()
 
 new Vue({

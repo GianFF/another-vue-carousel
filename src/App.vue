b50f6298b75788b309d9c15232aaa2507a7367fb
gian <gf.fioriello@gmail.com>
2019-05-13 13:15:09 -0300

    #2 - Added more configurations to the main pagea

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,72 @@
     </div>
 
     <div class="separator"> 
-      <hr class="first-line">
-      <hr class="second-line">
+      <hr class="first-line"><hr class="second-line">
     </div>
+    
+    <AVCarousel :itemsCount="items.length"
+              :itemsToShow="3" 
+              :itemsToSlide="2"
+
+              :itemWidth="202"  
+              :itemHeight="202"  
+              :itemMarginRight="10" 
+
+              :arrowMargin="0" 
+              :arrowWidth="21" 
+              :arrowHeight="50" 
+
+              :id="'first-carousel'">
+
+      <template v-slot:left-arrow>
+        <div class="bordered-div-arrow">0</div>
+      </template>
+              
+      <AVCarouselItem v-for="item in items" 
+                      v-bind:key="`item-${item}`" 
+                      :id="'first-carousel'">
+        <div class="bordered-div-item">{{item}}</div>
+      </AVCarouselItem>
+
+      <template v-slot:right-arrow>
+        <div class="bordered-div-arrow">0</div>
+      </template>
+    </AVCarousel>
+
+     <div class="separator"> 
+       <hr class="first-line"><hr class="second-line">
+      </div>
+
+    <AVCarousel :itemsCount="items.length"
+              :itemsToShow="4" 
+              :itemsToSlide="1"
+              :itemWidth="200" 
+              :itemHeight="200" 
+              :itemMarginRight="10" 
+              :arrowMargin="0" 
+              :arrowWidth="20" 
+              :arrowHeight="50" 
+              :id="'second-carousel'">
+      <template v-slot:left-arrow>
+        <MyArrow :navDirection="'left'"/>
+      </template>
+              
+      <AVCarouselItem v-for="item in items" 
+                      v-bind:key="`item-${item}`" 
+                      :id="'second-carousel'">
+        <MyItem :number="item"/>
+      </AVCarouselItem>
+
+      <template v-slot:right-arrow>
+        <MyArrow :navDirection="'right'"/>
+      </template>
+    </AVCarousel>
+
+    <div class="separator"> 
+      <hr class="first-line"><hr class="second-line">
+    </div>
+
+    <h1> Find and resolve the issue: </h1>
 
     <AVCarousel :itemsCount="items.length"
               :itemsToShow="3" 
@@ -20,19 +83,25 @@
               :arrowMargin="0" 
               :arrowWidth="20" 
               :arrowHeight="50" 
-              :id="'first-carousel'">
-      <template v-slot:left-arrow>
-        <MyArrow :navDirection="'left'"/>
+              :id="'third-carousel'">
+      <template v-slot:left-arrow class="bordered-div-arrow">
+        <div class="bordered-div-arrow">0</div>
       </template>
               
-      <AVCarouselItem v-for="item in items" v-bind:key="`item-${item}`" :id="'first-carousel'">
-        <MyItem/>
+      <AVCarouselItem v-for="item in items" 
+                      v-bind:key="`item-${item}`" 
+                      :id="'third-carousel'">
+        <div class="bordered-div-item">{{item}}</div>
       </AVCarouselItem>
 
       <template v-slot:right-arrow>
-        <MyArrow :navDirection="'right'"/>
+        <div class="bordered-div-arrow">0</div>
       </template>
     </AVCarousel>
+
+    <div class="separator"> 
+      <hr class="first-line"><hr class="second-line">
+    </div>
   </div>
 </template>
 
@@ -107,4 +176,38 @@ export default {
     background-color: #40B783;
     border: 1px solid #40B783;
   }
+
+
+ .bordered-div-item {
+    margin-right: 10px; 
+    width: 200px;
+    height: 200px;
+
+    border: 1px solid #40B783;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .bordered-div-arrow {
+    width: 20px;
+    height: 50px;
+
+    border: 1px solid #35495e;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }.bordered-div-arrow:hover {
+    cursor: pointer;
+  }
+
+  /*
+   * I have added a border of 1px to   
+   * to my carousel items and arrows.
+   *
+   * How many pixels of "width" does 
+   * my items and arrows have now?
+  */
 </style>
